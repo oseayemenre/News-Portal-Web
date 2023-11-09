4d29c437806edf906713ac1720e5eb3b01cffa1d
Ayemenre Oseghale <oseayemenre04@gmail.com>
2023-11-09 02:40:23 +0100

    another commit

--- client/app/(main)/contact/page.tsx
+++ client/app/(main)/contact/page.tsx
@@ -125,19 +125,19 @@ const ContactUs = () => {
             Contact Info
           </h2>
           <div className='flex gap-x-4 max-sm:gap-x-2 mt-12'>
-            <CiLocationOn size={24} color='white' />
+            <CiLocationOn size={24} color='white' className='max-sm:w-5' />
             <p className='text-white text-[18px] max-sm:text-[14px]'>
               PMB 4003, ILISHAN REMO, OGUN STATE, NIGERIA
             </p>
           </div>
           <div className='flex gap-x-4 max-sm:gap-x-2 mt-12'>
-            <BiEnvelope size={24} color='white' />
+            <BiEnvelope size={24} color='white' className='max-sm:w-5' />
             <p className='text-white text-[18px] max-sm:text-[14px]'>
               ABCDedfRoad@<span className='max-sm:block'>ABCD.com</span>
             </p>
           </div>
           <div className='flex gap-x-4 mt-12 mb-20 max-sm:gap-x-2'>
-            <FiPhoneCall size={24} color='white' />
+            <FiPhoneCall size={24} color='white' className='max-sm:w-5' />
             <p className='text-white text-[18px] max-sm:text-[14px]'>
               123-456-789
             </p>
